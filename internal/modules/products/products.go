96ad77937709818a801d322465830b7e053a536f
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-07-26 16:31:14 +0500

    feat: get all products

--- internal/modules/products/products.go
+++ internal/modules/products/products.go
@@ -1,1 +1,34 @@
 package products
+
+import (
+	"github.com/TauAdam/ecom-api/internal/models"
+	"github.com/TauAdam/ecom-api/shared/response"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Handler struct {
+	store models.ProductsStore
+}
+
+func NewHandler(store models.ProductsStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) InitRoutes(router *mux.Router) {
+	router.HandleFunc("/products", h.handleGetAllProducts).Methods(http.MethodGet)
+}
+
+func (h *Handler) handleGetAllProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := h.store.GetProducts()
+	if err != nil {
+		response.SendError(w, http.StatusInternalServerError, err)
+		return
+	}
+	err = response.SendJSON(w, http.StatusOK, products)
+	if err != nil {
+		log.Fatalf("failed to send response: %v", err)
+	}
+	return
+}
--- internal/modules/products/store.go
+++ internal/modules/products/store.go
@@ -1,6 +1,9 @@
 package products
 
-import "database/sql"
+import (
+	"database/sql"
+	"github.com/TauAdam/ecom-api/internal/models"
+)
 
 type Store struct {
 	db *sql.DB
@@ -8,4 +11,38 @@ type Store struct {
 
 func NewProductsStore(db *sql.DB) Store {
 	return Store{db: db}
+}
+
+func (s Store) GetProducts() ([]models.Product, error) {
+	rows, err := s.db.Query("SELECT * FROM products")
+	if err != nil {
+		return nil, err
+	}
+	products := make([]models.Product, 0)
+	for rows.Next() {
+		p, err := scanRowIntoProducts(rows)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, *p)
+	}
+	return products, nil
+}
+
+func scanRowIntoProducts(rows *sql.Rows) (*models.Product, error) {
+	product := new(models.Product)
+
+	err := rows.Scan(
+		&product.ID,
+		&product.Name,
+		&product.Description,
+		&product.Price,
+		&product.Image,
+		&product.Quantity,
+		&product.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
 }
