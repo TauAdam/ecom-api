adb193ca1bec676a9b9fc9b2edaf6761352f0a50
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-08-01 15:52:01 +0500

    feat: cart

--- internal/modules/cart/service.go
+++ internal/modules/cart/service.go
@@ -16,3 +16,49 @@ func getCartItemsIDs(items []models.CartItem) ([]int, error) {
 
 	return productIDs, nil
 }
+
+func (h *Handler) createOrder(products []models.Product, cartItems []models.CartItem, userID int) (int, float64, error) {
+	productsMap := make(map[int]models.Product)
+
+	for _, p := range products {
+		productsMap[p.ID] = p
+	}
+
+	if err := isItemsInStock(cartItems, productsMap); err != nil {
+		return 0, 0, err
+	}
+
+	// TODO: calculate total price
+
+	return 0, 0, nil
+}
+
+func isItemsInStock(items []models.CartItem, productsMap map[int]models.Product) error {
+	if len(items) == 0 {
+		return fmt.Errorf("cart is empty")
+	}
+
+	for _, item := range items {
+		product, ok := productsMap[item.ProductID]
+
+		if !ok {
+			return fmt.Errorf("product with id %d is not available in the shop", item.ProductID)
+		}
+
+		if product.Quantity < item.Quantity {
+			return fmt.Errorf("product %s is not available in the quantitiy requested", product.Name)
+		}
+	}
+
+	return nil
+}
+
+func calculateTotalPrice(cartItems []models.CartItem, products map[int]models.Product) float64 {
+	var total float64
+
+	for _, item := range cartItems {
+		product := products[item.ProductID]
+		total += product.Price * float64(item.Quantity)
+	}
+	return total
+}
